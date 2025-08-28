64f28d68c267671963fd012b5cc7ca9697219e71
biplob <biplobchondro749@gmail.com>
2025-08-28 22:18:50 +0600

    first commit

--- style.js
+++ style.js
@@ -1,10 +1,8 @@
 
-    // function getElement(id) {
-    //     const element = document.getElementById(id);
-    //         return element;
-    // }
-
-
+    function getElement(id) {
+        const element = document.getElementById(id);
+            return element;
+    }
 
 // -----------Love button
   const loveButton = document.getElementsByClassName('love-btn');
@@ -19,4 +17,29 @@
   }
   
 
+//-------------Call button
+const coinButton = getElement('coin-count');
+let coin = parseInt(coinButton.innerText);
+console.log(coin);
 
+const callButtons = document.getElementsByClassName('call-button')
+
+for(i = 0; i < callButtons.length; i++){
+  callButtons[i].addEventListener('click', function(e){
+    if (e.target.className.includes("call-button")){
+      const callButton = e.target;
+      const serviceName = callButton.parentNode.parentNode.children[1].children[1].innerText
+      const serviceNumber = callButton.parentNode.parentNode.children[1].children[2].children[0].innerText
+      console.log(serviceNumber);
+
+      if(coin >= 20){
+        alert(`Calling ${serviceName} Service : ${serviceNumber}`)
+        coin -= 20;
+        coinButton.innerText = coin;
+      }
+      else{
+        alert("আপনার পযাপ্ত কয়েন নেই, কল করতে কমপক্ষে ২০ কযেন লাগবে")
+      }
+    }
+  }
+)}
